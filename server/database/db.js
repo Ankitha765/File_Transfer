1895793d86bcd1ce3743e18b97e28bc1725e5ec7
Ankitha765 <ankithal765@gmail.com>
2024-09-27 00:03:28 +0530

    Few Changes on Project

--- server/database/db.js
+++ server/database/db.js
@@ -4,10 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const DBConnection = async () => {
-    const USERNAME = process.env.DB_USERNAME;
-    const PASSWORD = process.env.DB_PASSWORD;
-
-    const MONGO_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.tylba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const MONGO_URI = process.env.DB_URI;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
